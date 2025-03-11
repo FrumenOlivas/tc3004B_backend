805bb220191510ddfb3f98a275652d552b16fd93
Frumen Olivas <frumen@olivas.com>
2025-03-11 10:15:49 -0600

    firebase

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js";
 import items2Routes from "./routes/items2.routes.js";
+import items3Routes from "./routes/items3.routes.js";
 import loginRoutes from "./routes/login.routes.js";
 import morgan from "morgan";
 import { connectDB } from "./utils/mongodb.js";
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
 app.use(items2Routes);
+app.use(items3Routes);
 app.use(loginRoutes);
 
 app.listen(5000, console.log("http://localhost:5000"));
